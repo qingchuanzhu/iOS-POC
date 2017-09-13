887cda91b91fe48ead77fde9971047f0ea653fc8
Qingchuan Zhu <zqc@utexas.edu>
2017-09-13 14:34:27 -0700

    enable background fetch

--- BackgroundMode/TheBackgrounder/AppDelegate.swift
+++ BackgroundMode/TheBackgrounder/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
-    
+    UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
     
     return true
   }
