9aead4dd2b3cbe0ed741dc3e1e98537928a31ba0
Qingchuan Zhu <zqc@utexas.edu>
2017-09-18 21:53:45 -0700

    Added another framework target inside existing framework project

---
+++ Creating&Distributing iOS Framework/ZQCUIComponents/ZQCMiniUIComponents/ZQCMiniUIComponents.h
@@ -0,0 +1,19 @@
+//
+//  ZQCMiniUIComponents.h
+//  ZQCMiniUIComponents
+//
+//  Created by Qingchuan Zhu on 9/18/17.
+//  Copyright © 2017 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZQCMiniUIComponents.
+FOUNDATION_EXPORT double ZQCMiniUIComponentsVersionNumber;
+
+//! Project version string for ZQCMiniUIComponents.
+FOUNDATION_EXPORT const unsigned char ZQCMiniUIComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZQCMiniUIComponents/PublicHeader.h>
+
+
