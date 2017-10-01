6547b794e2a481a32b44fc5867871594eccc487e
Qingchuan Zhu <zqc@utexas.edu>
2017-10-01 16:02:57 -0700

    Added Swift4 Trail POC

---
+++ Swift4Trial/swift4trail/swift4trail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift4trail
+//
+//  Created by Qingchuan Zhu on 10/1/17.
+//  Copyright © 2017 Qingchuan Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
