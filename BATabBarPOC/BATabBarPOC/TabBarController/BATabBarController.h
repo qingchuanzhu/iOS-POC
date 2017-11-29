c72423c0f73c0eb8019c2adeaa51017414f0d29a
Qingchuan Zhu <zqc@utexas.edu>
2017-11-28 22:13:36 -0800

    Added the tab bar controller

---
+++ BATabBarPOC/BATabBarPOC/TabBarController/BATabBarController.h
@@ -0,0 +1,13 @@
+//
+//  BATabBarController.h
+//  BATabBarPOC
+//
+//  Created by Qingchuan Zhu on 11/28/17.
+//  Copyright © 2017 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BATabBarController : UITabBarController
+
+@end
