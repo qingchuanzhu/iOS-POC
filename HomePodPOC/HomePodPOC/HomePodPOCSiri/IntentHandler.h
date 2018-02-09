6ad8ddde46a7ec20dfe1a75128c929bb111e43b5
Qingchuan Zhu <zqc@utexas.edu>
2018-02-09 11:33:45 -0800

    Added the Sirikit Extension

---
+++ HomePodPOC/HomePodPOC/HomePodPOCSiri/IntentHandler.h
@@ -0,0 +1,13 @@
+//
+//  IntentHandler.h
+//  HomePodPOCSiri
+//
+//  Created by Qingchuan Zhu on 2/9/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <Intents/Intents.h>
+
+@interface IntentHandler : INExtension
+
+@end
---
+++ HomePodPOC/HomePodPOC/HomePodPOCSiri/IntentHandler.m
@@ -0,0 +1,26 @@
+//
+//  IntentHandler.m
+//  HomePodPOCSiri
+//
+//  Created by Qingchuan Zhu on 2/9/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import "IntentHandler.h"
+
+// As an example, this class is set up to handle Message intents.
+// You will want to replace this or add other intents as appropriate.
+// The intents you wish to handle must be declared in the extension's Info.plist.
+
+// You can test your example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+// "<myApp> John saying hello"
+// "Search for messages in <myApp>"
+
+@interface IntentHandler ()
+
+@end
+
+@implementation IntentHandler
+
+@end
