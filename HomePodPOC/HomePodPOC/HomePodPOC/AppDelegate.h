a038ed412bb93f042824f8fb4b76161c394cd2e3
Qingchuan Zhu <zqc@utexas.edu>
2018-02-09 11:27:18 -0800

    Created the starter for HomePod POC

---
+++ HomePodPOC/HomePodPOC/HomePodPOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomePodPOC
+//
+//  Created by Qingchuan Zhu on 2/9/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomePodPOC/HomePodPOC/HomePodPOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomePodPOC
+//
+//  Created by Qingchuan Zhu on 2/9/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
