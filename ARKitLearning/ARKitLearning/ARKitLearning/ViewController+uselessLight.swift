9e21ec946843dce602eff936be0d7be7feb99545
Qingchuan Zhu <zqc@utexas.edu>
2018-02-18 16:38:11 -0800

    refactor lightening method

---
+++ ARKitLearning/ARKitLearning/ARKitLearning/ViewController+uselessLight.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController+uselessLight.swift
+//  ARKitLearning
+//
+//  Created by Qingchuan Zhu on 2/18/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+import Foundation
