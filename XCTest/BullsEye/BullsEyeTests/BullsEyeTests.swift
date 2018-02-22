06bb863042e170229fe5e183c55b17ffc54bdd63
Qingchuan Zhu <zqc@utexas.edu>
2018-02-22 13:46:55 -0800

    Added test case for testing score computation

--- XCTest/BullsEye/BullsEyeTests/BullsEyeTests.swift
+++ XCTest/BullsEye/BullsEyeTests/BullsEyeTests.swift
@@ -7,17 +7,32 @@
 //
 
 import XCTest
+@testable import BullsEye
 
 class BullsEyeTests: XCTestCase {
+    
+    var gameUnderTest: BullsEyeGame!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        gameUnderTest = BullsEyeGame()
+        gameUnderTest.startNewGame()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        gameUnderTest = nil
         super.tearDown()
+    }
+    
+    func testScoresComputed() {
+        // 1. given
+        let guess = gameUnderTest.targetValue + 5
+        // 2. when
+        _ = gameUnderTest.check(guess: guess)
+        // 3. then
+        XCTAssertEqual(gameUnderTest.scoreRound, 95, "Score computed from guess is wrong")
     }
     
     func testExample() {
