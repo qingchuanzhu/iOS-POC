2bb1bb494f5f5ecc36a1ad630c44b51155a82a58
Qingchuan Zhu <zqc@utexas.edu>
2018-06-07 19:57:19 -0700

    Added chart view in the controller

--- BA360ChartsPOC/BA360ChartsPOC/BA360ChartsPOC/ViewControllers/ChartPOCViewController.swift
+++ BA360ChartsPOC/BA360ChartsPOC/BA360ChartsPOC/ViewControllers/ChartPOCViewController.swift
@@ -7,15 +7,40 @@
 //
 
 import UIKit
+import Charts
 
-class ChartPOCViewController: UIViewController {
+class ChartPOCViewController: UIViewController, ChartViewDelegate {
 
+    @IBOutlet var holderView: UIView!
+    var chartView: BA360ChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        chartView = BA360ChartView(frame: .zero)
+        chartView.delegate = self
+        chartView.dragEnabled = true
+        
+        chartView.rightAxis.enabled = false
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.removeAllLimitLines()
+        leftAxis.axisMaximum = 50
+        leftAxis.axisMinimum = 0
+        
+        constraintChartView()
     }
 
+    func constraintChartView() {
+        chartView.translatesAutoresizingMaskIntoConstraints = false
+        self.holderView.addSubview(chartView)
+        chartView.leftAnchor.constraint(equalTo: holderView.leftAnchor, constant: 0).isActive = true
+        chartView.rightAnchor.constraint(equalTo: holderView.rightAnchor, constant: 0).isActive = true
+        chartView.topAnchor.constraint(equalTo: holderView.topAnchor, constant: 0).isActive = true
+        chartView.bottomAnchor.constraint(equalTo: holderView.bottomAnchor, constant: 0).isActive = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
