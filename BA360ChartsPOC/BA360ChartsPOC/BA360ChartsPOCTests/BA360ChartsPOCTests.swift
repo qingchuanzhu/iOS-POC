9836a0f9cf4fb6b2445d40c379254dd318378257
Qingchuan Zhu <zqc@utexas.edu>
2018-06-12 11:03:39 -0700

    Added Unit test bundle

---
+++ BA360ChartsPOC/BA360ChartsPOC/BA360ChartsPOCTests/BA360ChartsPOCTests.swift
@@ -0,0 +1,35 @@
+//
+//  BA360ChartsPOCTests.swift
+//  BA360ChartsPOCTests
+//
+//  Created by Qingchuan Zhu on 6/12/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+import XCTest
+
+class BA360ChartsPOCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
