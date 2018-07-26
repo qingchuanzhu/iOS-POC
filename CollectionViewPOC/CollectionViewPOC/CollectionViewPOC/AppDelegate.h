9b8792081c8c76585c7125a3dcb455de55b1f04e
Qingchuan Zhu <zqc@utexas.edu>
2018-07-26 10:09:58 -0700

    Created Collection View POC slate

---
+++ CollectionViewPOC/CollectionViewPOC/CollectionViewPOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewPOC
+//
+//  Created by Qingchuan Zhu on 7/26/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
