5ae03202b73edf72856465c45795e42ecb28f192
Qingchuan Zhu <zqc@utexas.edu>
2018-07-26 10:32:19 -0700

    Created the collection view controller and basic set ups

---
+++ CollectionViewPOC/CollectionViewPOC/CollectionViewPOC/FlickrPhototsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlickrPhototsViewController.h
+//  CollectionViewPOC
+//
+//  Created by Qingchuan Zhu on 7/26/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrPhototsViewController : UICollectionViewController
+
+@end
---
+++ CollectionViewPOC/CollectionViewPOC/CollectionViewPOC/FlickrPhototsViewController.m
@@ -0,0 +1,101 @@
+//
+//  FlickrPhototsViewController.m
+//  CollectionViewPOC
+//
+//  Created by Qingchuan Zhu on 7/26/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import "FlickrPhototsViewController.h"
+
+@interface FlickrPhototsViewController ()
+
+@property (nonatomic, assign) UIEdgeInsets const sectionInsets;
+
+@end
+
+@implementation FlickrPhototsViewController
+
+static NSString * const reuseIdentifier = @"FlickrCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.sectionInsets = UIEdgeInsetsMake(50.0, 20.0, 50.0, 20.0);
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Register cell classes
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of items
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
