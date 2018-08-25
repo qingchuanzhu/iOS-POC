7578126d66025a6064c64849ab298c2de26b7a9b
Seven Yoona <sevenY@example.com>
2018-08-24 20:10:22 -0700

    Created the initial setup for the LinkedList test for Sequence protocol study

---
+++ Swift4Trial/LinkedList/LinkedList/List.swift
@@ -0,0 +1,29 @@
+//
+//  List.swift
+//  LinkedList
+//
+//  Created by Qingchuan Zhu on 8/24/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+import Foundation
+
+enum List<Element> {
+    case end
+    indirect case node(Element, next:List<Element>)
+}
+
+extension List{
+    func cons(_ x:Element) -> List {
+        return .node(x, next:self)
+    }
+}
+
+extension List:ExpressibleByArrayLiteral{
+    init(arrayLiteral elements: Element...) {
+        self = elements.reversed().reduce(.end) { (currList, e) -> List in
+            // equavalently currList.cons(e)
+            return .node(e, next:currList)
+        }
+    }
+}
---
+++ Swift4Trial/LinkedList/LinkedList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LinkedList
+//
+//  Created by Qingchuan Zhu on 8/24/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+import Foundation
+
+let list2:List = [3,2,1]
+print(list2)
